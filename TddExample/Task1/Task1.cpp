170920a1f3fa796c85e4efe2e8eda5e8154d4388
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2020-04-14 20:57:23 +0300

    Add files via upload

---
+++ TddExample/Task1/Task1.cpp
@@ -0,0 +1,13 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "indexer.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double* array = new double[4]{ 1, 2, 3, 4 };
+	indexer indexer1 = indexer(array, 4, 1, 2);
+
+}
